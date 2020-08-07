6cc34fe37e5c57641cafb4503f32b334a021cd91
unknown <domovionock8@gmail.com>
2020-08-07 13:04:14 +0500

    произведение обратной матрицы на вектор

--- 3.3 матрицы.py
+++ 3.3 матрицы.py
@@ -1,10 +1,14 @@
 import numpy as np
 import numpy.linalg as alg
 
-a = np.array([[-2, 0, 2.5, 0.3], [-8.5, 2.4, 1.6, -0.4], [-3.4, 0, 2.1, -4.8], [3.5, 8.2, 3, 4.6]])
-b = np.array([-1.8, -3.28, -0.5, -2.83])
+a = np.array([[-2, -8.5, -3.4, 3.5],
+              [0, 2.4, 0, 8.2],
+              [2.5, 1.6, 2.1, 3],
+              [0.3, -0.4, -4.8, 4.6]])
+
+b = np.array([-1.88, -3.28, -0.5, -2.83])
 
 a_inv = alg.inv(a)
-x = a_inv * b
+x = np.dot(a_inv, b)
 
 print(np.round(x, 1))
